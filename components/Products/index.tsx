d8085485066c5e002f4a33ef455a32f27bad205b
joelmatiasduran <joelmatiasduran@gmail.com>
2021-12-08 06:26:29 -0300

    Loading test

--- components/Products/index.tsx
+++ components/Products/index.tsx
@@ -3,6 +3,7 @@ import useSWR from 'swr'
 import { fetcher } from '../../utils/functions'
 import Image from 'next/image'
 import AeroCoin from '../../assets/icons/coin.svg'
+import Loading from '../Loading'
 
 interface ProductsProps {
   name: string
@@ -16,37 +17,40 @@ const Products: React.FC<ProductsProps> = () => {
 
   return (
     <>
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
-        {products
-          ? products.map((products, index) => {
-              return (
-                <div
+      <div
+        className={
+          products
+            ? 'grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6'
+            : 'w-full'
+        }
+      >
+        {products ? (
+          products.map((products, index) => {
+            return (
+              <div
+                key={index}
+                className="flex flex-col bg-clip-padding backdrop-filter backdrop-blur-xl bg-opacity-60 bg-white border border-gray-200 p-6 rounded-xl"
+              >
+                <h2>{products.name}</h2>
+                <Image
                   key={index}
-                  className="flex flex-col border-black border-4 p-6 rounded-xl"
-                >
-                  <h2>{products.name}</h2>
-                  <Image
-                    key={index}
-                    src={products.img.hdUrl}
-                    width={100}
-                    height={250}
-                  />
-                  <h3>{products.category}</h3>
-                  <button className="flex flex-col-reverse">
-                    <div className="flex flex-row content-center items-center justify-center py-6 bg-black hover:bg-aero-blue text-white rounded-lg duration-300">
-                      <h3>{products.cost}</h3>
-                      <Image
-                        src={AeroCoin}
-                        alt="Coins"
-                        width={50}
-                        height={50}
-                      />
-                    </div>
-                  </button>
-                </div>
-              )
-            })
-          : 'Loading..'}
+                  src={products.img.hdUrl}
+                  width={100}
+                  height={250}
+                />
+                <h3>{products.category}</h3>
+                <button className="flex flex-col-reverse">
+                  <div className="flex flex-row content-center items-center justify-center py-6 bg-black hover:bg-aero-blue text-white rounded-lg duration-300">
+                    <h3>{products.cost}</h3>
+                    <Image src={AeroCoin} alt="Coins" width={50} height={50} />
+                  </div>
+                </button>
+              </div>
+            )
+          })
+        ) : (
+          <Loading />
+        )}
       </div>
     </>
   )
